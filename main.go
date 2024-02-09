548e676a3857a3939b0d90d3f56a9330365e4a1a
kalebpc <113309697+kalebpc@users.noreply.github.com>
2024-02-09 11:36:22 -0500

    refactor main function abstract out image processing
    Signed-off-by: kalebpc <113309697+kalebpc@users.noreply.github.com>

--- main.go
+++ main.go
@@ -12,62 +12,66 @@ import (
 
 func main() {
 	arguments, exit := args.GetArgs()
-	for !exit {
-		file, err := os.Open(arguments.Infile)
+	if !exit {
+		processImage(arguments)
+	}
+	os.Exit(0)
+}
+
+func processImage(arguments args.Arguments) {
+	file, err := os.Open(arguments.Infile)
+	if err != nil {
+		fmt.Println("Error opening infile")
+	} else {
+		defer file.Close()
+		var imageedit imageedit.Imageedit
+		imageedit.Oldimg, err = png.Decode(file)
 		if err != nil {
-			fmt.Println("Error opening infile")
+			fmt.Println("Cannot decode file")
 		} else {
-			defer file.Close()
-			var imageedit imageedit.Imageedit
-			imageedit.Oldimg, err = png.Decode(file)
+			imageedit.Newimg = image.NewNRGBA(image.Rectangle{image.Point{imageedit.Oldimg.Bounds().Min.X, imageedit.Oldimg.Bounds().Min.Y}, image.Point{imageedit.Oldimg.Bounds().Max.X, imageedit.Oldimg.Bounds().Max.Y}})
+			// Apply imageedit method
+			switch arguments.Function {
+			case "FX":
+				// flip over x-axis
+				imageedit.FX()
+			case "FY":
+				// flip over y-axis
+				imageedit.FY()
+			case "FXY":
+				// rotate
+				imageedit.FXY()
+			case "RRX":
+				// roundrobin around x-axis
+				imageedit.RRX(arguments.Pixels)
+			case "RRY":
+				// roundrobin around y-axis
+				imageedit.RRY(arguments.Pixels)
+			case "RRR":
+				// roundrobin `pixels` size rows
+				imageedit.RRR(arguments.Pixels)
+			case "RRC":
+				// roundrobin `pixels` size columns
+				imageedit.RRC(arguments.Pixels)
+			case "PIX":
+				// `pixels` size pixelate whole image
+				imageedit.PIX(arguments.Pixels)
+			}
+			// create new file
+			newfile, err := os.Create(arguments.Outfile)
 			if err != nil {
-				fmt.Println("Cannot decode file")
+				fmt.Println("Error Creating new outfile")
 			} else {
-				imageedit.Newimg = image.NewNRGBA(image.Rectangle{image.Point{imageedit.Oldimg.Bounds().Min.X, imageedit.Oldimg.Bounds().Min.Y}, image.Point{imageedit.Oldimg.Bounds().Max.X, imageedit.Oldimg.Bounds().Max.Y}})
-				// Apply imageedit method
-				switch arguments.Function {
-				case "FX":
-					// flip over x-axis
-					imageedit.FX()
-				case "FY":
-					// flip over y-axis
-					imageedit.FY()
-				case "FXY":
-					// rotate
-					imageedit.FXY()
-				case "RRX":
-					// roundrobin around x-axis
-					imageedit.RRX(arguments.Pixels)
-				case "RRY":
-					// roundrobin around y-axis
-					imageedit.RRY(arguments.Pixels)
-				case "RRR":
-					// roundrobin `pixels` size rows
-					imageedit.RRR(arguments.Pixels)
-				case "RRC":
-					// roundrobin `pixels` size columns
-					imageedit.RRC(arguments.Pixels)
-				case "PIX":
-					// `pixels` size pixelate whole image
-					imageedit.PIX(arguments.Pixels)
-				}
-				// create new file
-				newfile, err := os.Create(arguments.Outfile)
+				defer newfile.Close()
+				// encode new file
+				err = png.Encode(newfile, imageedit.Newimg)
 				if err != nil {
-					fmt.Println("Error Creating new outfile")
+					fmt.Println("Error Encoding new image")
 				} else {
-					defer newfile.Close()
-					// encode new file
-					err = png.Encode(newfile, imageedit.Newimg)
-					if err != nil {
-						fmt.Println("Error Encoding new image")
-					} else {
-						fmt.Println("New Image Created!")
-					}
+					fmt.Println("New Image Created!")
 				}
 			}
 		}
-		exit = true
 	}
-	os.Exit(0)
+	return
 }
