825e518282ede50c6c6da60a038d008be302773c
kalebpc <113309697+kalebpc@users.noreply.github.com>
2024-02-13 11:08:06 -0500

    update method name call
    Signed-off-by: kalebpc <113309697+kalebpc@users.noreply.github.com>

--- main_test.go
+++ main_test.go
@@ -2,19 +2,19 @@ package main
 
 import (
 	"fmt"
+	"reflect"
 	"testing"
 )
 
 func BenchmarkMain(b *testing.B) {
 	imageedit := Imageedit{pixels: 33}
 	for j := 0; j < b.N; j += 1 {
-		// for i := 0; i < reflect.ValueOf(&imageedit).NumMethod(); i += 1 {
-		// function := reflect.TypeOf(&imageedit).Method(i).Name
-		function := "PIX"
-		arguments := []string{"./assets/dino.png", "./new.png", function}
-		processImage(arguments, imageedit)
-		fmt.Println("function: ", arguments[2])
-		fmt.Print("\n")
-		// }
+		for i := 0; i < reflect.ValueOf(&imageedit).NumMethod(); i += 1 {
+			function := reflect.TypeOf(&imageedit).Method(i).Name
+			arguments := []string{"./assets/dino.png", "./new.png", function}
+			processImage(arguments, imageedit)
+			fmt.Println("function: ", arguments[2])
+			fmt.Print("\n")
+		}
 	}
 }
